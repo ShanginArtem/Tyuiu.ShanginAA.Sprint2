8437f003999f807bfb7b67e0df312c064467a4d2
Tyuiu.ShanginAA@outlook.com <Tyuiu.ShanginAA@outlook.com>
2024-12-07 21:19:38 +0500

    task0v1

---
+++ Tyuiu.ShanginAA.Sprint2.Task0.V1.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+//Написать программу из операций сравнений (==, !=, <, >, <=, >=, последовательность операций не должна нарушаться)
+//    и арифметических выражений, которая вернет логическую последовательность(массив): 
+//   (True, False, True, False, True, False), при x = 233, y = 122
+
+
+namespace Tyuiu.ShanginAA.Sprint2.Task0.V1.Lib
+{
+    public class DataService : ISprint2Task0V1
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            bool[] res = new bool[6];
+
+            res[0] = x - 111 == y;
+            res[1] = x - 111 != y;
+            res[2] = y < x;
+            res[3] = y > x;
+            res[4] = x - 111 <= y;
+            res[5] = y >= x; 
+
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.ShanginAA.Sprint2.Task0.V1/Program.cs
@@ -0,0 +1,56 @@
+﻿using Tyuiu.ShanginAA.Sprint2.Task0.V1.Lib;
+
+namespace Tyuiu.ShanginAA.Sprint2.Task0.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int x = 233;
+            int y = 122;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+
+            Console.Title = "Спринт #2 | Выполнил: Шангин А. А. | ИСП6-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Операции сравнения                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Шангин Артём Александрович | ИСПб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,         *");
+            Console.WriteLine("* последовательность операций не должна нарушаться) и арифметических      *");
+            Console.WriteLine("* выражений, которая вернет логическую последовательность (массив):       *");
+            Console.WriteLine("* (True, False, True, False, True, False), при x = 233, y = 122           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            Console.WriteLine("X = " + x);
+            Console.WriteLine("Y = " + y);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
