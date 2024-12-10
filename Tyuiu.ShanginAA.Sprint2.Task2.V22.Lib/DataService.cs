61c7000a09fdc5f032aafa0505a6c0a8870d899b
Tyuiu.ShanginAA@outlook.com <Tyuiu.ShanginAA@outlook.com>
2024-12-11 00:59:49 +0500

    task2v22

---
+++ Tyuiu.ShanginAA.Sprint2.Task2.V22.Lib/DataService.cs
@@ -0,0 +1,42 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.ShanginAA.Sprint2.Task2.V22.Lib
+{
+    public class DataService : ISprint2Task2V22
+    {
+        public bool CheckDotInShadedArea(int x, int y)
+        {
+            bool res = false;
+
+            if ((y == 3 || y == 4) && ((x >= 3 && x <= 5 ) || (x >= 9 && x <= 12)))
+            {
+                res = true;
+            }
+            else if ((y >= 5 && y <= 7) && (x >= 3 && x <= 13) && (x != 13 && y == 5))
+            {
+                res = true;
+            }
+            
+            else if ((y == 8) && ((x >= 4 && x <= 6) || (x >= 9 && x <= 10) || (x >= 12 && x <= 13))) res = true;
+
+            else if ((y == 9) && ((x >= 4 && x <= 6) || (x >= 9 && x <= 10) || (x == 12))) res = true;
+
+            else if ((y == 10) && ((x >= 4 && x <= 6) || (x == 12))) res = true;
+
+            else if ((y == 11) && ((x >= 3 && x <= 6) || (x == 12))) res = true;
+
+            else if ((y == 12) && (x == 5)) res = true;
+
+            else if ((y == 13) && (x == 5)) res = true;
+
+            else if ((y == 14) && ((x >= 4 && x <= 10))) res = true;
+
+            
+            return res;
+
+
+
+           
+        }
+    }
+}
---
+++ Tyuiu.ShanginAA.Sprint2.Task2.V22/Program.cs
@@ -0,0 +1,59 @@
+﻿using Tyuiu.ShanginAA.Sprint2.Task2.V22.Lib;
+
+namespace Tyuiu.ShanginAA.Sprint2.Task2.V22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int x;
+            int y;
+
+            Console.Title = "Спринт #2 | Выполнил: Шангин А. А. | ИСП6-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Логические операции                                               *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #22                                                             *");
+            Console.WriteLine("* Выполнил: Шангин Артём Александрович | ИСПб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Написать программу на, которая запрашивает целые значения с клавиатуры  *");
+            Console.WriteLine("* и вычисляет находится ли точка с координатами X,Y в заштрихованной      *");
+            Console.WriteLine("* области.                                                                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("ВВЕДИТЕ ЗНАЧЕНИЕ X: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("ВВЕДИТЕ ЗНАЧЕНИЕ Y: ");
+            y = Convert.ToInt32(Console.ReadLine());
+            
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            if (res == true)
+            {
+                Console.WriteLine("Точка находится в заштрихованной области.");
+            }
+            else
+            {
+                Console.WriteLine("Точка не находится в заштрихованной области.");
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
